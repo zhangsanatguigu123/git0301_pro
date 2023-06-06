a3eeee49e7ccdfc7543160cced3235ae80bfa494
庞家伟 <15501149646@163.com>
2023-06-06 15:29:14 +0800

    idea  master测试分支

--- src/main/java/com/atguigu/Main.java
+++ src/main/java/com/atguigu/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("master");
         System.out.println("master3");
+        System.out.println("master master");
     }
 }
